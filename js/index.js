74f4f4b66915b493734cdac0e82ad84981c5f76c
Ygu4492 <ygu4492@conestogac.on.ca>
2024-03-06 19:16:29 -0500

    create the default task list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 $(function(){
-  
+  const defaultTaskList = [
+    {
+      taskName: "Create task form",
+      taskDesc:
+        "Create a form on the page, users should be able to add, edit, and delete tasks",
+      studentName: "Gu, Yunxiang",
+      studentNumber: "8094492",
+    },
+    {
+      taskName: "Store the tasks in localStorage",
+      taskDesc:
+        "Validate the task form values, push the task into the current task list and store it in localStorage",
+      studentName: "",
+      studentNumber: "",
+    },
+    {
+      taskName: "Display the tasks on the page",
+      taskDesc:
+        "When the page loads, it should check localStorage and display all the tasks on the task list panel",
+      studentName: "",
+      studentNumber: "",
+    },
+    {
+      taskName: "Task filter",
+      taskDesc:
+        "Create a search input, the user should be able to filter the task via contains a certain text",
+      studentName: "",
+      studentNumber: ""
+    },
+  ];
 });
